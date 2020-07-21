9de99cb833680ce8fe8c93282e96a98903f447b7
Kevin Buckley <kevin.buckley2013@gmail.com>
2020-07-21 07:14:01 -0400

    Refactors SideNav for position fixed and better responsive design

--- src/App.js
+++ src/App.js
@@ -11,57 +11,39 @@ import {
   MDBCardText,
 } from "mdbreact";
 import "./App.css";
-import nightSky from "./Night Sky 3.jpg";
 import Navigation from "./components/Navigation/Navigation";
 
 function App() {
   return (
     <MDBContainer>
-      <MDBRow className="text-center">
-        <MDBCol className="">
-          <Navigation />
-        </MDBCol>
-        <MDBCol className="">
-          <MDBBox>
-            <MDBCard className="border-0">
-              <MDBCardBody>
-                <MDBCardTitle id="name">Kevin Buckley</MDBCardTitle>
-                <MDBCardText>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card&apos;s content.
-                </MDBCardText>
-              </MDBCardBody>
-            </MDBCard>
-          </MDBBox>
-          <MDBBox>
-            <MDBCard className="border-0">
-              <MDBCardBody>
-                <MDBCardTitle>Card title</MDBCardTitle>
-                <MDBCardText>
-                  <img
-                    src={nightSky}
-                    alt="word"
-                    className="w-responsive text-center mx-auto p-3 mt-2"
-                  />
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card&apos;s content.
-                </MDBCardText>
-              </MDBCardBody>
-            </MDBCard>
-          </MDBBox>
-          <MDBBox>
-            <MDBCard className="border-0">
-              <MDBCardBody>
-                <MDBCardTitle>Card title</MDBCardTitle>
-                <MDBCardText>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card&apos;s content.
-                </MDBCardText>
-              </MDBCardBody>
-            </MDBCard>
-          </MDBBox>
-        </MDBCol>
-      </MDBRow>
+      <Navigation />
+      <MDBCard id="main" className="border-0">
+        <MDBCardBody>
+          <MDBCardTitle id="name">Kevin Buckley</MDBCardTitle>
+          <MDBCardText className="">
+            <h1>Check out my Github</h1>
+            <p>for projects I've made in my personal time</p>
+          </MDBCardText>
+        </MDBCardBody>
+      </MDBCard>
+      <MDBCard id="main" className="border-0">
+        <MDBCardBody>
+          <MDBCardTitle>Card title</MDBCardTitle>
+          <MDBCardText>
+            Some quick example text to build on the card title and make up the
+            bulk of the card&apos;s content.
+          </MDBCardText>
+        </MDBCardBody>
+      </MDBCard>
+      <MDBCard id="main" className="border-0">
+        <MDBCardBody>
+          <MDBCardTitle>Card title</MDBCardTitle>
+          <MDBCardText>
+            Some quick example text to build on the card title and make up the
+            bulk of the card&apos;s content.
+          </MDBCardText>
+        </MDBCardBody>
+      </MDBCard>
     </MDBContainer>
   );
 }
